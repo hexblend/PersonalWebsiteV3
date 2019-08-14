be67005932b29148e2e6db81311a7381a25b5511
Vlad Bibire <vladbbr1@gmail.com>
2019-08-15 00:13:27 +0100

    Passing the fields as props

--- web/app/themes/sage/resources/views/front-page.blade.php
+++ web/app/themes/sage/resources/views/front-page.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <landing-section video="@asset('video/sea.mp4')"></landing-section>
+    <landing-section
+            video="@asset('video/sea.mp4')"
+            name="{{$landing_fields->name}}"
+            slug="{{$landing_fields->slug}}"
+            slug_ending_1="{{$landing_fields->slug_ending_1}}"
+            slug_ending_2="{{$landing_fields->slug_ending_2}}"
+            slug_ending_3="{{$landing_fields->slug_ending_3}}"
+            button_text="{{$landing_fields->button_text}}"
+            bottom_text="{{$landing_fields->bottom_text}}"
+    ></landing-section>
 @endsection
