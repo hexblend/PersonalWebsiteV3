053fc3ce14749871044d056883199843b906d0d3
Vlad Bibire <vladbbr1@gmail.com>
2019-08-14 22:02:06 +0100

    Vue typer updated markup

--- web/app/themes/sage/resources/assets/js/components/Sections/LandingSection.vue
+++ web/app/themes/sage/resources/assets/js/components/Sections/LandingSection.vue
@@ -9,18 +9,34 @@
         <div class="landing__central-elements">
             <div class="landing__central-elements--logo"></div>
             <h1 class="landing__central-elements--title">Vlad Bibire</h1>
-            <h2 class="landing__central-elements--slug">Passionate about creating websites.</h2>
-            <a class="landing__central-elements--button btn btn-ghost" href="#about">Hire me</a>
+            <h2 class="landing__central-elements--slug">
+                Passionate about creating
+                <vue-typer
+                        :text='["websites.","web apps.","web platforms."]'
+                        initial-action='erasing'
+                        :pre-erase-delay='2500'
+                        :erase-delay='100'
+                        :pre-type-delay='100'
+                        :type-delay='100'
+                        erase-style='backspace'
+                ></vue-typer>
+            </h2>
+            <a class="landing__central-elements--button btn btn-ghost" href="#about">Meet me</a>
         </div>
         <p class="landing__footer">all made in Southampton with ❤️ and ☕️</p>
     </section>
 </template>
 
 <script>
+    import { VueTyper } from 'vue-typer'
     export default {
         name: 'landing-section',
         props: {
             'video': String
+        },
+        components: {
+            VueTyper
         }
     }
 </script>
+
