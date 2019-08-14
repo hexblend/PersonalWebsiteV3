c79d1d1764ce2f5e14434e9f7c2bd76c132f161a
Vlad Bibire <vladbbr1@gmail.com>
2019-08-14 21:08:58 +0100

    Gradient markup

--- web/app/themes/sage/resources/assets/js/components/Sections/LandingSection.vue
+++ web/app/themes/sage/resources/assets/js/components/Sections/LandingSection.vue
@@ -5,13 +5,14 @@
                 <source :src="video" type="video/mp4">
             </video>
         </div>
+        <div class="landing__video-gradient"></div>
         <div class="landing__central-elements">
             <div class="landing__central-elements--logo"></div>
             <h1 class="landing__central-elements--title">Vlad Bibire</h1>
             <h2 class="landing__central-elements--slug">Passionate about creating websites.</h2>
             <a class="landing__central-elements--button btn btn-ghost" href="#about">Hire me</a>
         </div>
-        <p class="landing__footer">All made in Southampton with ❤️ and ☕️</p>
+        <p class="landing__footer">all made in Southampton with ❤️ and ☕️</p>
     </section>
 </template>
 
