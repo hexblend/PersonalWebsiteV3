087d307823e3836a488d64b026f00c35e8c12316
Vlad Bibire <vladbbr1@gmail.com>
2019-08-14 20:26:14 +0100

    Landing Section Markup

--- web/app/themes/sage/resources/assets/js/components/Sections/LandingSection.vue
+++ web/app/themes/sage/resources/assets/js/components/Sections/LandingSection.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="landing-section">
-        
-    </div>
+    <section class="landing" id="landing">
+        <div class="landing__central-elements">
+            <div class="landing__central-elements--logo"></div>
+            <h1 class="landing__central-elements--title">Vlad Bibire</h1>
+            <h2 class="landing__central-elements--slug">Passionate about creating websites.</h2>
+            <a class="landing__central-elements--button btn btn-ghost" href="#about">Hire me</a>
+        </div>
+        <p class="landing__footer">All made in Southampton with ❤️ and ☕️</p>
+    </section>
 </template>
 
 <script>
     export default {
-        name: 'landing-section'
+        name: 'landing-section',
     }
 </script>
