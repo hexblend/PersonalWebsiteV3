158383c3e83f664e3e1beca590faca8bb1face77
Vlad Bibire <vladbbr1@gmail.com>
2019-08-14 17:52:26 +0100

    Cleaned Front Page template file from clutter to suit the app

--- web/app/themes/sage/resources/views/front-page.blade.php
+++ web/app/themes/sage/resources/views/front-page.blade.php
@@ -2,25 +2,6 @@
 
 @section('content')
 
-@include('partials.page-header')
-
-@if($custom_posts)
-    @foreach($custom_posts as $post)
-        <div>
-            <h3>{{ $post['title'] }}</h3>
-            <p>{{ $post['content'] }}</p>
-            @if($post['example_meta_field'])
-                <strong>Example meta field:</strong>
-                <p>{{ $post['example_meta_field'] }}</p>
-            @endif
-        </div>
-    @endforeach
-    @else
-    <div>
-        No custom posts found, try creating an example custom post at <a href="/wp/wp-admin/edit.php?post_type=my-custom-post-type">/wp-admin</a>.
-    </div>
-@endif
-
 <!-- Vue Main Component -->
 <hello-world class="mt-10"></hello-world>
 
