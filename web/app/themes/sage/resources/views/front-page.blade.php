1813dadd53a61c5ec44b0317d5a0da6f19d8f492
Vlad Bibire <vladbbr1@gmail.com>
2019-08-15 12:51:21 +0100

    Include projects section within the layout

--- web/app/themes/sage/resources/views/front-page.blade.php
+++ web/app/themes/sage/resources/views/front-page.blade.php
@@ -2,4 +2,7 @@
 
 @section('content')
     <landing-section video="@asset('video/sea.mp4')" landing_fields='{!! json_encode($landing_fields) !!}'></landing-section>
+    <div class="wrapper">
+        <projects-section></projects-section>
+    </div>
 @endsection
