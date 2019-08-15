5a0ce7a8264a06b0b731cf6e642f590891644189
Vlad Bibire <vladbbr1@gmail.com>
2019-08-16 00:01:11 +0100

    Project preview component markup

---
+++ web/app/themes/sage/resources/assets/js/components/ProjectPreview.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="project-preview">
+        <div class="project-preview__info">
+            <h5>Project title</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'project-preview',
+    }
+</script>
