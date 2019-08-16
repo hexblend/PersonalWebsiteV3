a3a5a31f5709cc2811c7ce212dceaf01051cd409
Vlad Bibire <vladbbr1@gmail.com>
2019-08-17 00:19:35 +0100

    pass testimonials object as props

--- web/app/themes/sage/resources/views/front-page.blade.php
+++ web/app/themes/sage/resources/views/front-page.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <landing-section video="@asset('video/sea.mp4')" landing_fields='{!! json_encode($landing_fields) !!}'></landing-section>
     <div class="wrapper">
-        <projects-section allprojects='{!! json_encode($projects) !!}'></projects-section>
+        <projects-section allProjects='{!! json_encode($projects) !!}'></projects-section>
+        <testimonials-section allTestimonials='{!! json_encode($testimonials) !!}'></testimonials-section>
     </div>
 @endsection
