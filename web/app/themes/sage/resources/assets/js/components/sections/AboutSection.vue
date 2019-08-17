0db65c9727baf0041fddcf495ff56bb7b34f54ba
Vlad Bibire <vladbbr1@gmail.com>
2019-08-17 16:26:12 +0100

    About Section front-end

---
+++ web/app/themes/sage/resources/assets/js/components/sections/AboutSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="about" id="about">
+        <div class="about__texture"></div>
+        <div class="about__content">
+            <div class="about__content--text">
+                <h3 class="about__heading">So... who am I?</h3>
+                <p>Enthusiastic web developer with a passion for creating intuitive web interfaces. Third-year student, studying Web Design & Development, improving my knowledge by developing complex personal projects and client solutions in my spare time.</p>
+                <p>Being a fast learner I can easily acquire new knowledge to integrate inside projects without losing my focus on the deadline. Working in small teams made me become a very communicative person with good leadership skills that can help to solve problems even in stressful conditions.</p>
+                <p>I am a perfectionist and I love challenges, the harder the milestone, the more I enjoy it.</p>
+            </div>
+            <div class="about__content--image"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'about-section',
+    }
+</script>
