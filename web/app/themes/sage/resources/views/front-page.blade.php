02a92997b092e5eaa119710b1a26945c8e777c87
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-21 18:38:08 +0100

    Add wrapper large/small class

--- web/app/themes/sage/resources/views/front-page.blade.php
+++ web/app/themes/sage/resources/views/front-page.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <landing-section video="@asset('video/sea.mp4')" landing_fields='{!! json_encode($landing_fields) !!}'></landing-section>
-    <div class="wrapper">
+    <div class="wrapper-small">
         <projects-section allProjects='{!! json_encode($projects) !!}'></projects-section>
         <testimonials-section allTestimonials='{!! json_encode($testimonials) !!}'></testimonials-section>
     </div>
     <about-section about_fields='{!! json_encode($about_fields) !!}'></about-section>
-    <div class="wrapper">
+    <div class="wrapper-large">
         <packages-section></packages-section>
     </div>
 @endsection
