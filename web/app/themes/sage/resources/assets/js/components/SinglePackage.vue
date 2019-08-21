48dd88518aa6413faa84faf2e437d20b24a638c4
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-21 18:40:32 +0100

    Single Package component front end markup

---
+++ web/app/themes/sage/resources/assets/js/components/SinglePackage.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+    <div class="single-package">
+        <div class="single-package__heading" style="max-width: 400px;">
+            <h3 class="single-package__heading--title">Web page for an Individual</h3>
+            <p class="single-package__heading--description">Ideal for a single professional to present his work in a modern way</p>
+        </div>
+        <div class="single-package__description">
+            <div class="single-package__description--column1">
+                <h5 class="single-package__description--column1-title">The service includes:</h5>
+                <p>- Initial meeting to perfectly understand your business model</p>
+                <p>- Wireframes ti picture how your website should look like</p>
+                <p>- Feedback & additions session</p>
+            </div>
+<!--            <div class="single-package__description&#45;&#45;column2">-->
+<!--                <p>- Designs of a multi-page website</p>-->
+<!--                <p>- Final feedback session on designs</p>-->
+<!--                <p>- Coding the website to include multiple user roles & ease in editing</p>-->
+<!--                <p>- Final feedback on website's features</p>-->
+<!--                <p>- Training session</p>-->
+<!--            </div>-->
+        </div>
+        <div class="single-package__button--placeholder"></div>
+        <button class="single-package__button btn btn-filled">Get in touch</button>
+    </div>
+    <div class="single-package">
+        <div class="single-package__heading">
+            <h3 class="single-package__heading--title">Web page for an Individual</h3>
+            <p class="single-package__heading--description">Ideal for a single professional to present his work in a modern way</p>
+        </div>
+        <div class="single-package__description">
+            <div class="single-package__description--column1">
+                <h5 class="single-package__description--column1-title">The service includes:</h5>
+                <p>- Initial meeting to perfectly understand your business model</p>
+                <p>- Wireframes ti picture how your website should look like</p>
+                <p>- Feedback & additions session</p>
+            </div>
+            <div class="single-package__description--column2">
+                <p>- Designs of a multi-page website</p>
+                <p>- Final feedback session on designs</p>
+                <p>- Coding the website to include multiple user roles & ease in editing</p>
+                <p>- Final feedback on website's features</p>
+                <p>- Training session</p>
+            </div>
+        </div>
+        <div class="single-package__button--placeholder"></div>
+        <button class="single-package__button btn btn-filled">Get in touch</button>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'single-package'
+    }
+</script>
