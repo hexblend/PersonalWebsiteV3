1d4ab32c2bd97b7d3390574e8abf12fce170480e
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-21 18:40:17 +0100

    Packages section front-end markup

---
+++ web/app/themes/sage/resources/assets/js/components/Sections/PackagesSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="packages" id="packages">
+        <h3 class="packages__heading">Let's collaborate!</h3>
+        <p class="packages__subheading">Have an idea for a project? Check out my services and get in touch</p>
+        <div class="packages__wrapper">
+            <single-package></single-package>
+        </div>
+    </section>
+</template>
+
+<script>
+    import SinglePackage from "../SinglePackage";
+    export default {
+        name: 'packages-section',
+        components: {SinglePackage},
+    }
+</script>
