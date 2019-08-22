adfd7a013ecf8230df9c9f89ceebd9f238a50737
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-22 15:44:42 +0100

    Remove hard coded content

--- web/app/themes/sage/resources/assets/js/components/SinglePackage.vue
+++ web/app/themes/sage/resources/assets/js/components/SinglePackage.vue
@@ -1,47 +1,31 @@
 <template>
-    <div>
-    <div class="single-package">
-        <div class="single-package__heading" style="max-width: 400px;">
-            <h3 class="single-package__heading--title">Web page for an Individual</h3>
-            <p class="single-package__heading--description">Ideal for a single professional to present his work in a modern way</p>
-        </div>
-        <div class="single-package__description">
-            <div class="single-package__description--column1">
-                <h5 class="single-package__description--column1-title">The service includes:</h5>
-                <p>- Initial meeting to perfectly understand your business model</p>
-                <p>- Wireframes ti picture how your website should look like</p>
-                <p>- Feedback & additions session</p>
-            </div>
-        </div>
-        <a href="#about" class="single-package__button btn btn-filled">Get in touch</a>
-    </div>
     <div class="single-package">
         <div class="single-package__heading">
-            <h3 class="single-package__heading--title">Web page for an Individual</h3>
-            <p class="single-package__heading--description">Ideal for a single professional to present his work in a modern way</p>
+            <h3 class="single-package__heading--title">{{ spackage.title }}</h3>
+            <p class="single-package__heading--description">{{ spackage.subtitle }}</p>
         </div>
         <div class="single-package__description">
             <div class="single-package__description--column1">
-                <h5 class="single-package__description--column1-title">The service includes:</h5>
-                <p>- Initial meeting to perfectly understand your business model</p>
-                <p>- Wireframes ti picture how your website should look like</p>
-                <p>- Feedback & additions session</p>
-                <p>- Designs of a multi-page website</p>
+                <h5 class="single-package__description--column1-title">{{ spackage.description_title }}</h5>
+                <div v-html="spackage.description_column_1">
+                    {{ spackage.description_column_1 }}
+                </div>
             </div>
-            <div class="single-package__description--column2">
-                <p>- Final feedback session on designs</p>
-                <p>- Coding the website to include multiple user roles & ease in editing</p>
-                <p>- Final feedback on website's features</p>
-                <p>- Training session</p>
+            <div class="single-package__description--column2" v-if="spackage.description_column_2">
+                <div v-html="spackage.description_column_2">
+                    {{ spackage.description_column_2 }}
+                </div>
             </div>
         </div>
-        <a href="#about" class="single-package__button btn btn-filled">Get in touch</a>
-    </div>
+        <a href="#about" class="single-package__button btn btn-filled">{{ spackage.button_text }}</a>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'single-package'
+        name: 'single-package',
+        props: {
+            spackage: Object
+        },
     }
 </script>
