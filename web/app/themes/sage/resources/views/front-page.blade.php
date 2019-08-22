f252a49a94992bc0842d46d2dedde06fd45b7e4e
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-22 12:19:42 +0100

    Add custom fields to main work section

--- web/app/themes/sage/resources/views/front-page.blade.php
+++ web/app/themes/sage/resources/views/front-page.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.app')
 @section('content')
-    <landing-section video="@asset('video/sea.mp4')" landing_fields='{!! json_encode($landing_fields) !!}'></landing-section>
+    <landing-section
+        video="@asset('video/sea.mp4')"
+        landing_fields='{!! json_encode($landing_fields) !!}'
+    ></landing-section>
+
     <div class="wrapper-small">
-        <projects-section allProjects='{!! json_encode($projects) !!}'></projects-section>
-        <testimonials-section allTestimonials='{!! json_encode($testimonials) !!}'></testimonials-section>
+        <projects-section
+            work_fields='{!! json_encode($work_fields) !!}'
+            allProjects='{!! json_encode($projects) !!}'
+        ></projects-section>
+
+        <testimonials-section
+            allTestimonials='{!! json_encode($testimonials) !!}'
+        ></testimonials-section>
     </div>
-    <about-section about_fields='{!! json_encode($about_fields) !!}'></about-section>
+
+    <about-section
+        about_fields='{!! json_encode($about_fields) !!}'
+    ></about-section>
+
     <div class="wrapper-large">
         <packages-section></packages-section>
     </div>
