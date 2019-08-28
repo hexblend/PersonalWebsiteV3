83343b8c75c0d191e9fea67ada8ab8b04ffedcfb
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-28 13:29:06 +0100

    Cleaned some code

--- web/app/themes/sage/resources/assets/js/components/SinglePackage.vue
+++ web/app/themes/sage/resources/assets/js/components/SinglePackage.vue
@@ -7,14 +7,10 @@
         <div class="single-package__description">
             <div class="single-package__description--column1">
                 <h5 class="single-package__description--column1-title">{{ spackage.description_title }}</h5>
-                <div v-html="spackage.description_column_1">
-                    {{ spackage.description_column_1 }}
-                </div>
+                <div v-html="spackage.description_column_1"></div>
             </div>
             <div class="single-package__description--column2" v-if="spackage.description_column_2">
-                <div v-html="spackage.description_column_2">
-                    {{ spackage.description_column_2 }}
-                </div>
+                <div v-html="spackage.description_column_2"></div>
             </div>
         </div>
         <a href="#contact" class="single-package__button btn btn-filled" @click="getPackageTitle(spackage.title)">{{ spackage.button_text }}</a>
