c9ebe7d57add0d3a60c947877fd3da6f63b313d3
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-28 12:16:38 +0100

    Add no opener to links

--- web/app/themes/sage/resources/assets/js/components/Sections/ContactSection.vue
+++ web/app/themes/sage/resources/assets/js/components/Sections/ContactSection.vue
@@ -9,9 +9,9 @@
                 <h3 class="contact__information--links__heading">{{ fields.social_media_title }}</h3>
                 <div v-for="(link, index) in links" :key="index" class="contact__information--links__row">
                     <p class="contact__information--links__row--service">{{ link.title + ':' }}</p>
-                    <a v-if="link.URL" class="contact__information--links__row--link" :href="link.URL">{{ link.link_body }}</a>
-                    <a v-else-if="link.phone" class="contact__information--links__row--link" :href="'tel:' + link.phone">{{ link.link_body }}</a>
-                    <a v-else="link.email" class="contact__information--links__row--link" :href="'mailto:' + link.email">{{ link.link_body }}</a>
+                    <a v-if="link.URL" class="contact__information--links__row--link" rel="noopener" target="_blank"  :href="link.URL">{{ link.link_body }}</a>
+                    <a v-else-if="link.phone" class="contact__information--links__row--link" rel="noopener" :href="'tel:' + link.phone">{{ link.link_body }}</a>
+                    <a v-else="link.email" class="contact__information--links__row--link" rel="noopener" :href="'mailto:' + link.email">{{ link.link_body }}</a>
                 </div>
             </div>
         </div>
