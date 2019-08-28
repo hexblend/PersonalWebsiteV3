e2ae9a14089a260939b2b905ee8302593e31fdb6
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-08-28 04:08:51 +0100

    removed mounted function

--- web/app/themes/sage/resources/assets/js/components/Sections/ContactSection.vue
+++ web/app/themes/sage/resources/assets/js/components/Sections/ContactSection.vue
@@ -7,15 +7,12 @@
             <div class="contact__information--form" v-html="fields.form"></div>
             <div class="contact__information--links">
                 <h3 class="contact__information--links__heading">{{ fields.social_media_title }}</h3>
-
                 <div v-for="(link, index) in links" :key="index" class="contact__information--links__row">
                     <p class="contact__information--links__row--service">{{ link.title + ':' }}</p>
                     <a v-if="link.URL" class="contact__information--links__row--link" :href="link.URL">{{ link.link_body }}</a>
                     <a v-else-if="link.phone" class="contact__information--links__row--link" :href="'tel:' + link.phone">{{ link.link_body }}</a>
                     <a v-else="link.email" class="contact__information--links__row--link" :href="'mailto:' + link.email">{{ link.link_body }}</a>
                 </div>
-
-
             </div>
         </div>
     </section>
@@ -33,9 +30,6 @@
                 fields: JSON.parse(this.contact_fields),
                 links: JSON.parse(this.alllinks),
             }
-        },
-        mounted() {
-            console.log(this.links);
         }
     }
 </script>
