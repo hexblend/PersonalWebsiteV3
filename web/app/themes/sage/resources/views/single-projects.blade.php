948f93c4c90fea0999c011abe6e51ac108df7db8
vladbbr <vladbbr1@gmail.com>
2019-12-04 14:05:26 +0000

    Single Project Page syntax

--- web/app/themes/sage/resources/views/single-projects.blade.php
+++ web/app/themes/sage/resources/views/single-projects.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ get_the_title() }}</h1>
-    {!! get_field("project_case_study") !!}
+    <div class="singleProject__banner" style="background-image: url('{{get_field("project_banner_image")["url"]}}')">
+        <h2 class="singleProject__banner--title">{{ get_the_title() }} </h2>
+        <p class="singleProject__banner--smallDesc">{{ get_field("project_small_description") }}</p>
+    </div>
+    <div class="singleProject__content mx-auto">
+        {!! get_field("project_case_study") !!}
+    </div>
 @endsection
